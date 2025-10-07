58f9fa89e2b64f0d25cbaafda891cffa7c044208
riasc <richard.schaefer@zoho.com>
2025-10-07 18:58:01 -0500

    resolve install and added all.hpp header files

---
+++ include/genogrove/structure/all.hpp
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Indentifier: MIT
+ *
+ * Copyright (c) 2025 Richard A. Schäfer
+ *
+ * This file is part of genogrove and is licensed under the terms of the MIT license.
+ * See the LICENSE file in the root of the repository for more information.
+ */
+
+#ifndef GENOGROVE_ALL_HPP
+#define GENOGROVE_ALL_HPP
+
+#include <genogrove/structure/grove/grove.hpp>
+#include <genogrove/structure/grove/node.hpp>
+
+#endif //GENOGROVE_ALL_HPP
