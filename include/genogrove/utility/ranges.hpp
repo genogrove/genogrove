5f21491253ab565646425b79103485d9f7033930
riasc <richard.schaefer@zoho.com>
2025-12-01 20:52:27 -0600

    include optional

--- include/genogrove/utility/ranges.hpp
+++ include/genogrove/utility/ranges.hpp
@@ -9,48 +9,50 @@
 #ifndef UTILITY_RANGES_HPP
 #define UTILITY_RANGES_HPP
 
-    #include <ranges>
-    #include <algorithm>
-    namespace genogrove::utility {
-        namespace ranges = std::ranges;
-        namespace views = std::ranges::views;
-        using ranges::find;
-        using ranges::find_if;
+#include <ranges>
+#include <algorithm>
+#include <optional>
 
-        /*
-         * @brief Lookup a key in an associative container by its value.
-         * @param The associative container.
-         * @param The value to lookup.
-         * @return Type of std::optional<key_type> - key_type if the key is found, else std::nullopt
-         */
-        template<typename AssocContainer, typename Value>
-        auto key_lookup(const AssocContainer& container, const Value& value)
-        -> std::optional<typename AssocContainer::key_type> {
-            auto it = genogrove::utility::ranges::find_if(container, [&](const auto &p) {
-                return p.second == value;
-            });
-            if (it != genogrove::utility::ranges::end(container)) {
-                return it->first;
-            }
-            return std::nullopt;
-        }
+namespace genogrove::utility {
+    namespace ranges = std::ranges;
+    namespace views = std::ranges::views;
+    using ranges::find;
+    using ranges::find_if;
 
-        /*
-         * @brief Lookup a value in an associative container by its key
-         * @param The associative container.
-         * @param The key to lookup.
-         * @return Type of std::optional<mapped_type> - mapped_type if the key is found, else std::nullopt
-         */
-        template<typename AssocContainer, typename Key>
-        auto value_lookup(const AssocContainer& container, const Key& key)
-        -> std::optional<typename AssocContainer::mapped_type> {
-            auto it = genogrove::utility::ranges::find(container, key, [](const auto &p) {
-                return p.first;
-            });
-            if (it != genogrove::utility::ranges::end(container)) {
-                return it->second;
-            }
-            return std::nullopt;
+    /*
+     * @brief Lookup a key in an associative container by its value.
+     * @param The associative container.
+     * @param The value to lookup.
+     * @return Type of std::optional<key_type> - key_type if the key is found, else std::nullopt
+     */
+    template<typename AssocContainer, typename Value>
+    auto key_lookup(const AssocContainer& container, const Value& value)
+    -> std::optional<typename AssocContainer::key_type> {
+        auto it = genogrove::utility::ranges::find_if(container, [&](const auto &p) {
+            return p.second == value;
+        });
+        if (it != genogrove::utility::ranges::end(container)) {
+            return it->first;
         }
+        return std::nullopt;
     }
+
+    /*
+     * @brief Lookup a value in an associative container by its key
+     * @param The associative container.
+     * @param The key to lookup.
+     * @return Type of std::optional<mapped_type> - mapped_type if the key is found, else std::nullopt
+     */
+    template<typename AssocContainer, typename Key>
+    auto value_lookup(const AssocContainer& container, const Key& key)
+    -> std::optional<typename AssocContainer::mapped_type> {
+        auto it = genogrove::utility::ranges::find(container, key, [](const auto &p) {
+            return p.first;
+        });
+        if (it != genogrove::utility::ranges::end(container)) {
+            return it->second;
+        }
+        return std::nullopt;
+    }
+}
 #endif
