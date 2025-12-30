aa9a9e14922c33f372434314aaedd9ae4070e9d9
riasc <richard.schaefer@zoho.com>
2025-12-29 22:46:17 -0600

    missing namespace added

--- cli/include/handlers/bed.hpp
+++ cli/include/handlers/bed.hpp
@@ -18,19 +18,20 @@
 
 namespace gdt = genogrove::data_type;
 namespace ggs = genogrove::structure;
+namespace gio = genogrove::io;
 
 namespace handlers {
 namespace bed {
 
 // Insert BED file entries into a grove
 void grove_insert(
-    ggs::grove<gdt::interval, bed_entry>& grove,
+    ggs::grove<gdt::interval, gio::bed_entry>& grove,
     const std::string& filepath
 );
 
 // Intersect BED query file against a populated grove
 void grove_intersect(
-    const ggs::grove<gdt::interval, bed_entry>& grove,
+    const ggs::grove<gdt::interval, gio::bed_entry>& grove,
     const std::string& queryfile,
     std::ostream& output
 );
