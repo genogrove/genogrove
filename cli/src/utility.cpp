1d2861ce3ee2a3ff2566f5ccaa5c11780af862dd
riasc <richard.schaefer@zoho.com>
2026-01-07 13:48:08 -0600

    make std::localtime threadsafe for different platforms

--- cli/src/utility.cpp
+++ cli/src/utility.cpp
@@ -11,8 +11,15 @@ std::string util::get_time() {
     std::time_t now_c = std::chrono::system_clock::to_time_t(now);
 
     // convert the time and data to a string in the format YYYY-MM-DD
+    // use thread-safe localtime_r (POSIX) or localtime_s (Windows)
+    std::tm tm_buf;
+#ifdef _WIN32
+    localtime_s(&tm_buf, &now_c);
+#else
+    localtime_r(&now_c, &tm_buf);
+#endif
     std::stringstream ss;
-    ss << std::put_time(std::localtime(&now_c), "%Y-%m-%d %H:%M:%S");
+    ss << std::put_time(&tm_buf, "%Y-%m-%d %H:%M:%S");
     return ss.str();
 }
 
